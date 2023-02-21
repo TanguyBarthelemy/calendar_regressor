23e1524201217826a51b73722e80aa793d0c4cce
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-02-21 19:31:29 +0100

    changes number into integer adds SAS and rjd regressor computing

--- R/create_french_calendar.R
+++ R/create_french_calendar.R
@@ -1,10 +1,10 @@
 
 create_annual_calendar <- function(leap_year = FALSE) {
-    month_length <- c(31, 28 + leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    month_length <- c(31L, 28L + leap_year, 31L, 30L, 31L, 30L, 31L, 31L, 30L, 31L, 30L, 31L)
     month_name_vect <- seq(as.Date("0-01-01"), as.Date("0-12-01"), by = "month") |> format("%B")
     
     annual_cal <- data.frame(month_name = rep(x = month_name_vect, times = month_length), 
-                             month_number = rep(x = 1:12, times = month_length), 
+                             month_number = rep(x = 1L:12L, times = month_length), 
                              leap_year = leap_year) |> 
         dplyr::group_by(month_name) |>  
         dplyr::mutate(month_day_number = dplyr::row_number())
@@ -12,44 +12,44 @@ create_annual_calendar <- function(leap_year = FALSE) {
     return(annual_cal)
 }
 
-create_empty_calendar <- function(start = 1950, end = 2022, starting_day = "dimanche") {
+create_empty_calendar <- function(start = 1950L, end = 2022L, starting_day = "dimanche") {
     
     starting_day <- tolower(starting_day)
     weekday_en <- c("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday")
     weekday_fr <- c("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi")
     
-    if (starting_day %in% 1:7) {
+    if (starting_day %in% 1L:7L) {
         message("Tu as choisi le ", weekday_fr[starting_day], " comme jour de début.")
         index_day <- starting_day
         starting_day <- weekday_fr[starting_day]
     } else if (starting_day %in% weekday_fr) {
-        index_day <- (1:7)[weekday_fr == starting_day]
+        index_day <- (1L:7L)[weekday_fr == starting_day]
     } else if (starting_day %in% weekday_en) {
-        index_day <- (1:7)[weekday_en == starting_day]
+        index_day <- (1L:7L)[weekday_en == starting_day]
         starting_day <- weekday_fr[index_day]
     } else { 
-        stop("L'argument starting_day doit être dans la liste suivante :", paste0(c(weekday_fr, weekday_en, 1:7), collapse = ", "))
+        stop("L'argument starting_day doit être dans la liste suivante :", paste0(c(weekday_fr, weekday_en, 1L:7L), collapse = ", "))
     }
     
     weekday_name_vect <- weekday_fr
     
     year <- start:end
-    bissextile <- (year %% 400 == 0) | (year %% 4 == 0 & year %% 100 != 0)
+    bissextile <- (year %% 400L == 0L) | (year %% 4L == 0L & year %% 100L != 0L)
     
     empty_cal <- do.call(rbind, purrr::map2(.x = year, .y = bissextile, 
                                             .f = \(x, y) cbind(year = x, create_annual_calendar(leap_year = y)))) |> 
         dplyr::mutate(
-            week_number = as.integer(c(rep(seq_len(dplyr::n() %/% 7), each = 7), 
-                            rep(dplyr::n() %/% 7, times = dplyr::n() %% 7))), 
+            week_number = as.integer(c(rep(seq_len(dplyr::n() %/% 7L), each = 7L), 
+                                       rep(dplyr::n() %/% 7L, times = dplyr::n() %% 7L))), 
             Date = as.Date(paste(year, sprintf("%02.f", month_number), sprintf("%02.f", month_day_number), sep = "-")), 
-            quarter_number = as.integer(((month_number - 1) %/% 3) + 1)
+            quarter_number = as.integer(((month_number - 1L) %/% 3L) + 1L)
         ) |> 
         dplyr::group_by(year, month_name, month_number) |> 
         dplyr::mutate(NbDays = dplyr::n()) |> 
         dplyr::ungroup() |> 
         dplyr::mutate(
-            temp_nb_day_tot = 0:(dplyr::n() - 1) + index_day, 
-            weekday_number = (temp_nb_day_tot - 1) %% 7 + 1, 
+            temp_nb_day_tot = seq_len(dplyr::n()) - 1L + index_day, 
+            weekday_number = (temp_nb_day_tot - 1L) %% 7L + 1L, 
             weekday_name = weekday_name_vect[weekday_number]
         )
     
@@ -60,7 +60,7 @@ add_new_year <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             new_year = dplyr::case_when(
-                year >= 1811 & month_number == 1 & month_day_number == 1 ~ TRUE, 
+                year >= 1811L & month_number == 1L & month_day_number == 1L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -70,7 +70,7 @@ add_may_day <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             may_day = dplyr::case_when(
-                year >= 1947 & month_number == 5 & month_day_number == 1 ~ TRUE, 
+                year >= 1947L & month_number == 5L & month_day_number == 1L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -80,7 +80,7 @@ add_victory_day <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             victory_day = dplyr::case_when(
-                (year >= 1982 | (year %in% 1953:1958)) & month_number == 5 & month_day_number == 8 ~ TRUE, 
+                (year >= 1982L | (year %in% 1953L:1958L)) & month_number == 5L & month_day_number == 8L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -90,7 +90,7 @@ add_fete_nationale <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             fete_nationale = dplyr::case_when(
-                year >= 1880 & month_number == 7 & month_day_number == 14 ~ TRUE, 
+                year >= 1880L & month_number == 7L & month_day_number == 14L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -100,7 +100,7 @@ add_assumption <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             assumption = dplyr::case_when(
-                year >= 1638 & month_number == 8 & month_day_number == 15 ~ TRUE, 
+                year >= 1638L & month_number == 8L & month_day_number == 15L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -110,7 +110,7 @@ add_all_saints_day <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             all_saints_day = dplyr::case_when(
-                year >= 1801 & month_number == 11 & month_day_number == 1 ~ TRUE, 
+                year >= 1801L & month_number == 11L & month_day_number == 1L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -120,7 +120,7 @@ add_armistice <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             armistice = dplyr::case_when(
-                year >= 1922 & month_number == 11 & month_day_number == 11 ~ TRUE, 
+                year >= 1922L & month_number == 11L & month_day_number == 11L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -130,7 +130,7 @@ add_christmas <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate( 
             christmas = dplyr::case_when(
-                year >= 1802 & month_number == 12 & month_day_number == 25 ~ TRUE, 
+                year >= 1802L & month_number == 12L & month_day_number == 25L ~ TRUE, 
                 TRUE ~ FALSE)
         )
     return(full_calendar)
@@ -139,26 +139,26 @@ add_christmas <- function(calendar) {
 compute_easter <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate(
-            n_cycle_meton = year %% 19, 
-            c = year %/% 100, 
-            u = year %% 100, 
-            s_bissextile = c %/% 4,
-            t_bissextile = c %% 4,
-            p_cycle_proemptose = (c + 8) %/% 25,
-            q_proemptose = (c - p_cycle_proemptose + 1) %/% 3, 
-            e_epacte = (19 * n_cycle_meton + c - s_bissextile - q_proemptose + 15) %% 30, 
-            b_bissextile = u %/% 4, 
-            d_bissextile = u %% 4, 
-            L_dominicale = (2 * t_bissextile + 2 * b_bissextile - e_epacte - d_bissextile + 32) %% 7, 
-            h_correction = (n_cycle_meton + 11 * e_epacte + 22 * L_dominicale) %/% 451,
+            n_cycle_meton = year %% 19L, 
+            c = year %/% 100L, 
+            u = year %% 100L, 
+            s_bissextile = c %/% 4L,
+            t_bissextile = c %% 4L,
+            p_cycle_proemptose = (c + 8L) %/% 25L,
+            q_proemptose = (c - p_cycle_proemptose + 1L) %/% 3L, 
+            e_epacte = (19L * n_cycle_meton + c - s_bissextile - q_proemptose + 15L) %% 30L, 
+            b_bissextile = u %/% 4L, 
+            d_bissextile = u %% 4L, 
+            L_dominicale = (2L * t_bissextile + 2L * b_bissextile - e_epacte - d_bissextile + 32L) %% 7L, 
+            h_correction = (n_cycle_meton + 11L * e_epacte + 22L * L_dominicale) %/% 451L,
             
-            month_easter = (e_epacte + L_dominicale - 7 * h_correction + 114) %/% 31,
-            day_easter = (e_epacte + L_dominicale - 7 * h_correction + 114) %% 31 + 1
+            month_easter = (e_epacte + L_dominicale - 7L * h_correction + 114L) %/% 31L,
+            day_easter = (e_epacte + L_dominicale - 7L * h_correction + 114L) %% 31L + 1L
         ) |> 
         dplyr::group_by(year) |> 
         dplyr::mutate(
             easter = dplyr::case_when(
-                year >= 1583 & month_day_number == day_easter & month_number == month_easter ~ TRUE, 
+                year >= 1583L & month_day_number == day_easter & month_number == month_easter ~ TRUE, 
                 TRUE ~ FALSE)
         ) |> 
         dplyr::ungroup() |> 
@@ -172,9 +172,9 @@ add_easter_monday <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::group_by(year) |> 
         dplyr::mutate(
-            day_easter_monday = max(easter * temp_nb_day_tot + 1), 
+            day_easter_monday = max(easter * temp_nb_day_tot + 1L), 
             easter_monday = dplyr::case_when(
-                year >= 1801 & temp_nb_day_tot == day_easter_monday ~ TRUE, 
+                year >= 1801L & temp_nb_day_tot == day_easter_monday ~ TRUE, 
                 TRUE ~ FALSE
             )
         ) |> 
@@ -187,9 +187,9 @@ add_ascension <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::group_by(year) |> 
         dplyr::mutate(
-            day_ascension = max(easter * temp_nb_day_tot + 39), 
+            day_ascension = max(easter * temp_nb_day_tot + 39L), 
             ascension = dplyr::case_when(
-                year >= 1801 & temp_nb_day_tot == day_ascension ~ TRUE, 
+                year >= 1801L & temp_nb_day_tot == day_ascension ~ TRUE, 
                 TRUE ~ FALSE
             )
         ) |> 
@@ -202,11 +202,11 @@ add_whit_monday <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::group_by(year) |> 
         dplyr::mutate(
-            day_whit_monday = max(easter * temp_nb_day_tot + 50), 
+            day_whit_monday = max(easter * temp_nb_day_tot + 50L), 
             whit_monday = dplyr::case_when(
-                year == 2005 & temp_nb_day_tot == day_whit_monday ~ 0.5,
-                year >= 1886 & temp_nb_day_tot == day_whit_monday ~ 1., 
-                TRUE ~ 0.
+                year == 2005L & temp_nb_day_tot == day_whit_monday ~ .5,
+                year >= 1886L & temp_nb_day_tot == day_whit_monday ~ 1, 
+                TRUE ~ 0
             )
         ) |> 
         dplyr::ungroup() |> 
@@ -217,16 +217,16 @@ add_whit_monday <- function(calendar) {
 add_in_off <- function(calendar) {
     full_calendar <- calendar |> 
         dplyr::mutate(
-            Day = 1, 
+            Day = 1L, 
             Off = pmin(new_year + may_day + victory_day + easter_monday + 
                            ascension + whit_monday + fete_nationale + 
                            assumption + all_saints_day + armistice + 
-                           christmas, 1), 
+                           christmas, 1L), 
             In = Day - Off) |> 
         dplyr::group_by(week_number) |> 
         dplyr::mutate(
-            v1 = max(weekday_name == "jeudi" & Off != 0) & max(weekday_name == "vendredi" & In != 0), 
-            v2 = max(weekday_name == "mardi" & Off != 0) & max(weekday_name == "lundi" & In != 0)
+            v1 = max(weekday_name == "jeudi" & Off != 0L) & max(weekday_name == "vendredi" & In != 0L), 
+            v2 = max(weekday_name == "mardi" & Off != 0L) & max(weekday_name == "lundi" & In != 0L)
         ) |> 
         dplyr::ungroup() |> 
         dplyr::mutate(
@@ -267,16 +267,16 @@ summarise_by_period <- function(calendar, frequency = "mensuelle") {
     
     frequency <- tolower(frequency)
     
-    if (frequency %in% c(4, 12)) {
-        message("Tu as choisi la fréquence ", c("mensuelle", "trimestrielle")[1 + (frequency == 4)], ".")
+    if (frequency %in% c(4L, 12L)) {
+        message("Tu as choisi la fréquence ", c("mensuelle", "trimestrielle")[1L + (frequency == 4L)], ".")
         frequency_num <- frequency |> as.integer()
     } else if (frequency %in% c("mensuelle", "trimestrielle")) {
-        frequency_num <- c(12, 4)[c("mensuelle", "trimestrielle") == frequency]
+        frequency_num <- c(12L, 4L)[c("mensuelle", "trimestrielle") == frequency]
     } else if (frequency %in% c("monthly", "quaterly")) {
-        frequency_num <- c(12, 4)[c("monthly", "quaterly") == frequency]
+        frequency_num <- c(12L, 4L)[c("monthly", "quaterly") == frequency]
     } else { 
         stop("L'argument frequency doit être dans la liste suivante :", 
-             paste0(c("mensuelle", "trimestrielle", "monthly", "quaterly", 4, 12), collapse = ", "))
+             paste0(c("mensuelle", "trimestrielle", "monthly", "quaterly", 4L, 12L), collapse = ", "))
     }
     
     full_calendar <- calendar |> 
@@ -288,7 +288,7 @@ summarise_by_period <- function(calendar, frequency = "mensuelle") {
         dplyr::group_by(year, periode) |> 
         dplyr::summarise(dplyr::across(.cols = -c(NbDays, leap_year, Date, EasterG, easter, 
                                                   weekday_name, quarter_number, month_number, 
-                                                  month_name, temp_nb_day_tot, month_day_number, week_number, weekday_number), sum, na.rm = TRUE), 
+                                                  month_name, temp_nb_day_tot, month_day_number, week_number, weekday_number),  \(x) sum(x, na.rm = TRUE)), 
                          NbDays = length(NbDays), LeapYear = dplyr::first(leap_year), 
                          month_number = dplyr::first(month_number), 
                          quarter_number = dplyr::first(quarter_number), 
@@ -312,8 +312,8 @@ add_means <- function(summarised_calendar) {
             corr = val - mean) |>  
         dplyr::ungroup() |> 
         dplyr::mutate(type = dplyr::case_when(
-            substr(type, 4, 4) == "0" ~ substr(type, 1, 3), 
-            substr(type, 3, 3) == "0" ~ substr(type, 1, 2), 
+            substr(type, 4L, 4L) == "0" ~ substr(type, 1L, 3L), 
+            substr(type, 3L, 3L) == "0" ~ substr(type, 1L, 2L), 
             TRUE ~ type
         )) |> 
         tidyr::pivot_wider(names_from = type, 
@@ -341,7 +341,7 @@ format_to_sas <- function(summarised_calendar, frequency = "mensuelle") {
             TD = In2 + In3 + In4 + In5 + In6, 
             WeekDays = TD - 5/2 * (PH + Day1 + Day7), 
             Bridges = monday_bridge + friday_bridge, 
-            LeapYear = (month_number == 2) * (LeapYear - 0.25)) |> 
+            LeapYear = (month_number == 2L) * (LeapYear - .25)) |> 
         dplyr::rename(month = month_number, 
                       qtr = quarter_number, 
                       Monday_B = monday_bridge, Friday_B = friday_bridge) |> 
@@ -358,7 +358,7 @@ format_to_sas <- function(summarised_calendar, frequency = "mensuelle") {
 }
 
 create_french_calendar <- function(
-        start = 1950, end = 2022, starting_day = "dimanche", 
+        start = 1950L, end = 2022L, starting_day = "dimanche", 
         summary = TRUE, by = "month", mean_correction = FALSE) {
     
     if (end < start) {
@@ -370,9 +370,9 @@ create_french_calendar <- function(
     
     if (summary) {
         if (by %in% c("month", "mois")) {
-            calendar <- calendar |> summarise_by_period(frequency = 12)
+            calendar <- calendar |> summarise_by_period(frequency = 12L)
         } else if (by %in% c("quarter", "trimestre")) {
-            calendar <- calendar |> summarise_by_period(frequency = 4)
+            calendar <- calendar |> summarise_by_period(frequency = 4L)
         } else {
             stop("L'argument frequency doit être dans la liste suivante :", 
                  paste0(c("mois", "trimestre", "month", "quater"), collapse = ", "))
@@ -387,7 +387,7 @@ create_french_calendar <- function(
 }
 
 replicate_sas_calendar <- function(
-        start = 1950, end = 2022, starting_day = "dimanche", 
+        start = 1950L, end = 2022L, starting_day = "dimanche", 
         summary = TRUE, by = "month") {
     calendar <- create_french_calendar(start = start, end = end, starting_day = starting_day, summary = TRUE, by = "month") |> 
         format_to_sas()
@@ -395,6 +395,19 @@ replicate_sas_calendar <- function(
     return(calendar)
 }
 
-cal1 <- create_french_calendar(end = 1960, by = "month", mean_correction = TRUE)
+
+cal1 <- cal1 |> 
+    dplyr::mutate(
+        G1 = In2_corr + In3_corr + In4_corr + In5_corr + In6_corr, 
+        G0 = Off_corr + In1_corr + In7_corr, 
+        REG1_SAS = G1 - G0 * 5 / 9, 
+        REG1_RJD = G1 - G0 * 5 / 2, 
+        Date = as.Date(paste(year, sprintf("%02.f", periode), "01", sep = "-"))) |> 
+    dplyr::select(Date, REG1_SAS, REG1_RJD)
+
+write.table(cal1, sep = ";", file = "./output/exp_reg1.csv", row.names = FALSE)
+
 cal2 <- create_french_calendar(summary = FALSE, end = 1960)
 cal3 <- replicate_sas_calendar()
+
+
