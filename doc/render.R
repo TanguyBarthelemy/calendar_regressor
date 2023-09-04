65f6f9cc901199c7e10c972e36c3ca35a8e4ebf9
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-09-04 17:53:04 +0200

    update documentation about calendar regressor

--- doc/render.R
+++ doc/render.R
@@ -1,2 +1,10 @@
 
-rmarkdown::render("./doc/Calendar steps.Rmd", output_format = "all", output_dir = "./doc")
+rmarkdown::render("./doc/Calendar steps.Rmd", 
+                  output_format = "all", output_dir = "./doc")
+
+# rmarkdown::render("./doc/Calendar steps.Rmd", 
+#                   output_format = "html_document", output_dir = "./doc")
+# rmarkdown::render("./doc/Calendar steps.Rmd",
+#                   output_format = "pdf_document", output_dir = "./doc")
+# rmarkdown::render("./doc/Calendar steps.Rmd", 
+#                   output_format = "github_document", output_dir = "./doc")
