8f7433cda9fb89de281e65ff4af4eb06f3f4973a
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-09-05 13:34:28 +0200

    modifs

--- doc/render.R
+++ doc/render.R
@@ -6,5 +6,5 @@ rmarkdown::render("./doc/Calendar steps.Rmd",
 #                   output_format = "html_document", output_dir = "./doc")
 # rmarkdown::render("./doc/Calendar steps.Rmd",
 #                   output_format = "pdf_document", output_dir = "./doc")
-# rmarkdown::render("./doc/Calendar steps.Rmd", 
+# rmarkdown::render("./doc/Calendar steps.Rmd",
 #                   output_format = "github_document", output_dir = "./doc")
