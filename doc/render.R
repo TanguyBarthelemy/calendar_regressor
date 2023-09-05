4878172d7c93dc058e0c02f83da88d03a64e1349
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-09-05 19:16:22 +0200

    update doc

--- doc/render.R
+++ doc/render.R
@@ -6,6 +6,8 @@ rmarkdown::render("./doc/Calendar-steps.Rmd",
     output_format = "all", output_dir = "./doc"
 )
 
+source("./doc/modify_gh_doc.R", encoding = "UTF-8")
+
 # rmarkdown::render("./doc/Calendar-steps.Rmd",
 #                   output_format = "html_document", output_dir = "./doc")
 # rmarkdown::render("./doc/Calendar-steps.Rmd",
--- doc/replace-math.lua
+++
@@ -1,7 +0,0 @@
-function Math(elem)
-  if elem.mathtype == "DisplayMath" then
-    return pandoc.RawBlock('markdown', '```math\n' .. elem.text .. '```\n')
-  else
-    return elem
-  end
-end
