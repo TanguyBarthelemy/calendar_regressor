b27a6226c23d789730dbd85192e5aa6aed25a0f4
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-09-05 18:56:21 +0200

    modifs

---
+++ doc/modify_gh_doc.R
@@ -0,0 +1,34 @@
+file <- "./doc/Calendar-steps.md"
+
+# Lire le contenu du Rmd
+input <- readLines(con = file, warn = FALSE, encoding = "UTF-8")
+
+output <- ""
+# Parcourir les lignes
+for (line in input) {
+    if (grepl("^\\$\\$", line)) {
+        
+        new_line <- gsub(pattern = "^\\$\\$|\\$\\$$", 
+                         replacement = "", x = line)
+        
+        output <- paste0(output, paste("```math", new_line, "```", sep = "\n"), "\n")
+        
+    } else {
+        output <- paste0(output, line, "\n")
+    }
+}
+
+writeLines(text = output, con = file)
+
+
+
+
+# Lire le contenu du Rmd
+input <- readLines(file, warn = FALSE, encoding = "UTF-8")
+
+# Remplacer les occurrences de $...$ par $`...`$
+output <- gsub("\\$([^$]+)\\$", "$`\\1`$", input)
+
+# Écrire le contenu modifié dans un nouveau fichier
+cat(output, file = file, sep = "\n")
+
