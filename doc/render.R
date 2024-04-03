05fa3aa88aae51472f450e81a6ba252d06498edc
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-04-03 15:17:44 +0200

    change rendering ??

--- doc/render.R
+++ doc/render.R
@@ -2,8 +2,11 @@
 ########                 Programme de render du document                ########
 ################################################################################
 
-rmarkdown::render(input = "./doc/Calendar-steps.Rmd",
-    output_format = "all", output_dir = "./doc"
+rmarkdown::render(
+    input = "./doc/Calendar-steps.Rmd",
+    output_format = "all",
+    output_dir = "./doc",
+    knit_root_dir = ".."
 )
 
 source("./doc/modify_gh_doc.R", encoding = "UTF-8")
@@ -12,10 +15,3 @@ remove_double_dollar(file)
 modify_simple_dollar(file)
 modify_table(file)
 modify_sum(file)
-
-# rmarkdown::render(input = "./doc/Calendar-steps.Rmd",
-#                   output_format = "html_document", output_dir = "./doc")
-# rmarkdown::render(input = "./doc/Calendar-steps.Rmd",
-#                   output_format = "pdf_document", output_dir = "./doc")
-# rmarkdown::render(input = "./doc/Calendar-steps.Rmd",
-#                   output_format = "github_document", output_dir = "./doc")
